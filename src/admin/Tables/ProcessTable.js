6feb4a185f32c2cf79fddd0328e07ad246a78ae8
imihirjain <jmanav251@gmail.com>
2024-11-28 21:33:07 +0530

    first commit

---
+++ src/admin/Tables/ProcessTable.js
@@ -0,0 +1,299 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function ProcessTable() {
+  const [submittedData, setSubmittedData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedEntry, setSelectedEntry] = useState(null);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  useEffect(() => {
+    fetchSubmittedData();
+  }, []);
+
+  const fetchSubmittedData = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:4000/api/lots/completed/process",
+        {
+          withCredentials: true,
+        }
+      );
+      console.log("Fetched Data:", response.data);
+      const sortedData = response.data.sort((a, b) => {
+        const dateA = new Date(a.submittedAt);
+        const dateB = new Date(b.submittedAt);
+        return isNaN(dateB.getTime()) - isNaN(dateA.getTime()) || dateB - dateA;
+      });
+      setSubmittedData(sortedData);
+    } catch (error) {
+      console.error("Error fetching submitted data:", error);
+    }
+  };
+
+  const handleEditClick = (entry, subEntry) => {
+    setSelectedEntry({ ...entry, subEntry });
+    setShowEditModal(true);
+  };
+
+  const handleDeleteClick = (entry, subEntry) => {
+    setSelectedEntry({ ...entry, subEntry });
+    setShowDeleteModal(true);
+  };
+
+  const handleDeleteConfirm = async () => {
+    try {
+      await axios.delete(
+        `http://localhost:4000/api/lots/${selectedEntry.subEntry._id}`
+      );
+      fetchSubmittedData();
+      setShowDeleteModal(false);
+    } catch (error) {
+      console.error("Error deleting entry:", error);
+    }
+  };
+
+  const handleEditConfirm = async () => {
+    try {
+      const updatedEntry = {
+        ...selectedEntry,
+        ...selectedEntry.subEntry,
+        kg: selectedEntry.subEntry.kg,
+        meter: selectedEntry.subEntry.meter,
+        roll: selectedEntry.subEntry.roll,
+      };
+
+      await axios.put(
+        `http://localhost:4000/api/lots/${selectedEntry.subEntry._id}`,
+        updatedEntry
+      );
+      fetchSubmittedData();
+      setShowEditModal(false);
+    } catch (error) {
+      console.error(
+        "Error updating entry:",
+        error.response ? error.response.data : error.message
+      );
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setSelectedEntry({
+      ...selectedEntry,
+      subEntry: { ...selectedEntry.subEntry, [name]: value },
+    });
+  };
+
+  const groupEntries = (data) => {
+    const groupedData = {};
+    data.forEach((item) => {
+      const key = `${item.partyName}-${item.qualityChallanNumber}`;
+      if (!groupedData[key]) {
+        groupedData[key] = [];
+      }
+      groupedData[key].push(item);
+    });
+    return groupedData;
+  };
+
+  const handleSearch = () => {
+    const filteredData = submittedData.filter(
+      (item) =>
+        item.partyName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        item.qualityChallanNumber
+          .toLowerCase()
+          .includes(searchQuery.toLowerCase()) ||
+        item.lotNumber.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    return filteredData;
+  };
+
+  const formatDateTime = (dateTimeString) => {
+    if (!dateTimeString || isNaN(new Date(dateTimeString).getTime())) {
+      return "Invalid Date";
+    }
+
+    const options = {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    };
+    return new Date(dateTimeString).toLocaleString("en-US", options);
+  };
+
+  const groupedData = groupEntries(handleSearch());
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="w-full mt-6 bg-white border-nav border-2 rounded-lg">
+        <div className="flex ml-4 mt-4 justify-between items-center">
+          <div className="text-title font-bold">
+            Process Table <br /> Total: {submittedData.length}
+          </div>
+
+          <div className="flex items-center bg-backgrnd justify-center mr-6 h-[35px] overflow-hidden rounded-full">
+            <div>
+              <img
+                className="h-[24px] w-[24px] ml-5"
+                src={require("../../assets/searchicon.png")}
+                alt="Inventory Management System"
+              />
+            </div>
+            <div className="h-[25px] ml-6 border-total border-[1px]"></div>
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              placeholder="Search..."
+              className="mb-4 mt-3 w-[250px] bg-backgrnd placeholder:text-center border border-none placeholder:font-login placeholder:text-[14px] placeholder:bg-backgrnd placeholder:text-total font-medium"
+            />
+          </div>
+        </div>
+
+        {Object.keys(groupedData).length === 0 ? (
+          <div className="text-center">
+            <p
+              className="text-gray-800 font-semibold text-lg"
+              style={{ color: "#4A90E2" }}
+            >
+              Sorry, no data available at the moment.
+            </p>
+          </div>
+        ) : (
+          <div className="overflow-x-auto">
+            <table className="min-w-full divide-y mt-6 divide-gray-200 border overflow-hidden">
+              <thead className="bg-header text-header-font font-header">
+                <tr>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-[14px] uppercase">
+                    Party Name
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-[14px] uppercase">
+                    Lot Number
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-[14px] uppercase">
+                    Shade
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-[14px] uppercase">
+                    Process
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-[14px] uppercase">
+                    Status
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-[14px] uppercase">
+                    Quality
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-[14px] uppercase">
+                    Challan Number
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-[14px] uppercase">
+                    Kg
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-[14px] uppercase">
+                    Meter
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-[14px] uppercase">
+                    Roll
+                  </th>
+                  {/* <th className="px-6 py-3 text-left text-xs font-semibold text-[14px] uppercase">
+                    Submitted Date & Time
+                  </th> */}
+                  <th className="px-12 py-3 text-left text-xs font-semibold text-[14px] uppercase">
+                    Actions
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-login font-header text-[16px] font-medium divide-gray-200">
+                {submittedData.map((entry, index) => (
+                  <React.Fragment key={index}>
+                    {entry.entries.map((subEntry, subIndex) => (
+                      <tr key={subEntry._id}>
+                        {subIndex === 0 && (
+                          <>
+                            <td
+                              className="px-6 py-4 whitespace-nowrap"
+                              rowSpan={entry.entries.length}
+                            >
+                              {entry.partyName}
+                            </td>
+                            <td
+                              className="px-6 py-4 whitespace-nowrap"
+                              rowSpan={entry.entries.length}
+                            >
+                              {entry.lotNumber || "N/A"}
+                            </td>
+                            <td
+                              className="px-6 py-4 whitespace-nowrap"
+                              rowSpan={entry.entries.length}
+                            >
+                              {entry.shade || "N/A"}
+                            </td>
+                            <td
+                              className="px-6 py-4 whitespace-nowrap"
+                              rowSpan={entry.entries.length}
+                            >
+                              {entry.processType || "N/A"}
+                            </td>
+                            <td
+                              className="px-6 py-4 whitespace-nowrap"
+                              rowSpan={entry.entries.length}
+                            >
+                              {entry.status || "N/A"}
+                            </td>
+                            <td
+                              className="px-6 py-4 whitespace-nowrap"
+                              rowSpan={entry.entries.length}
+                            >
+                              {entry.quality || "N/A"}
+                            </td>
+                          </>
+                        )}
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          {subEntry.challanNumber || "N/A"}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          {subEntry.kg || "N/A"}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          {subEntry.meter || "N/A"}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          {subEntry.roll || "N/A"}
+                        </td>
+                        {/* <td className="px-6 py-4 whitespace-nowrap">
+                          {subEntry.submittedAt
+                            ? formatDateTime(subEntry.submittedAt)
+                            : "N/A"}
+                        </td> */}
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <button
+                            className="inline-flex mt-1 px-4 py-2 text-sm font-medium font-login text-white bg-darkgray rounded-md hover:bg-white hover:text-darkgray outline"
+                            onClick={() => handleEditClick(entry, subEntry)}
+                          >
+                            Edit
+                          </button>
+                          <button
+                            className="inline-flex mt-1 ml-3 px-4 py-2 text-sm font-medium font-login text-white bg-red-700 rounded-md hover:bg-white hover:text-darkgray hover:outline outline-red-800"
+                            onClick={() => handleDeleteClick(entry, subEntry)}
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </React.Fragment>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ProcessTable;
---
+++ src/components/SideMenu.js
@@ -0,0 +1,195 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+function SideMenu() {
+  const [profileImageUrl, setProfileImageUrl] = useState("");
+  const [error, setError] = useState(null);
+  const localStorageData = JSON.parse(localStorage.getItem("user"));
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    if (!storedToken) {
+      setError("Token is missing. Please log in again.");
+    }
+
+    if (localStorageData) {
+      fetchProfileImage(localStorageData.firstName, localStorageData.lastName);
+    }
+  }, [localStorageData]);
+
+  const fetchProfileImage = (firstName, lastName) => {
+    const imageUrl = `https://api.dicebear.com/5.x/initials/svg?seed=${firstName}%20${lastName}`;
+    setProfileImageUrl(imageUrl);
+  };
+
+  const menuItems = [
+    {
+      to: "/dashboard-account",
+      label: "Dashboard",
+      icon: require("../assets/home.png"),
+    },
+    {
+      to: "/account-stock-in",
+      label: "Stock In",
+      icon: require("../assets/stockIn.png"),
+    },
+    {
+      to: "/account-in-table",
+      label: "Stock In Table",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/account-out",
+      label: "Stock Out",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/account-out-table",
+      label: "Stock Out Table",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/grey-stock",
+      label: "Grey Stock IN",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/grey-stock-out",
+      label: "Grey Stock Out",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/heat",
+      label: "Heatset",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/heat-table",
+      label: "Heatset Table",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/process",
+      label: "Process",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/process-table",
+      label: "Process Table",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/finish",
+      label: "Finish",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/finish-table",
+      label: "Finish Table",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/dispatch",
+      label: "Dispatch",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/dispatch-table",
+      label: "Dispatch Table",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/create-party",
+      label: "Create Party",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/create-user",
+      label: "Create User",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/admin-account-in-table",
+      label: "Accounts In Data",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/admin-grey-stock-out",
+      label: "Grey Stock Data",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/admin-heat-table",
+      label: "Heatset Data",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/admin-process-table",
+      label: "Process Data",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/admin-finish-table",
+      label: "Finish Data",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/admin-dispatch-table",
+      label: "Dispatch Data",
+      icon: require("../assets/stockInTable.png"),
+    },
+    {
+      to: "/admin-account-out-table",
+      label: "Accounts Out Data",
+      icon: require("../assets/stockInTable.png"),
+    },
+  ];
+
+  return (
+    <div className="h-full w-120 mt-5 flex-col justify-between bg-nav hidden lg:flex shadow-md shadow-gray-600 font-login">
+      <div className="mt-4">
+        <nav aria-label="Main Nav" className="flex flex-col space-y-1">
+          {menuItems.map((menuItem, index) => (
+            <Link
+              key={index}
+              to={menuItem.to}
+              className="flex items-center gap-2 rounded-lg hover:bg-login hover:text-white px-3 py-2"
+            >
+              <img
+                alt={menuItem.label}
+                src={menuItem.icon}
+                className="flex items-center gap-2"
+              />
+              <span className="text-[16px] font-medium hover:bg-login hover:text-white font-login">
+                {menuItem.label}
+              </span>
+            </Link>
+          ))}
+        </nav>
+      </div>
+      {/* Profile section */}
+      {/* <div className="fixed inset-x-0 bottom-0 w-[265px] h-[90px]">
+        <Link to="/profile-page">
+          <div className="flex items-center gap-2 bg-foooter_bg p-4 hover:bg-gray-50">
+            <img
+              alt="Profile"
+              src={profileImageUrl || require("../assets/footer_img.png")}
+              className="h-[48px] w-[48px] rounded-full object-cover"
+            />
+            <div className="font-semibold font-footer text-[14px]">
+              <p className="text-xs">
+                <strong className="block">
+                  {localStorageData?.firstName} {localStorageData?.lastName}
+                </strong>
+                <span className="text-footer_red">View Profile</span>
+              </p>
+            </div>
+          </div>
+        </Link>
+      </div>
+      {error && <div className="text-red-500 text-center mt-2">{error}</div>} */}
+    </div>
+  );
+}
+
+export default SideMenu;
